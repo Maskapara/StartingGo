904d60028370cb6515a6dc3391790ab726cce42d
MasuCatz <bloodteam.a.ranger@gmail.com>
2022-10-13 01:50:46 +0900

    hoge

---
+++ chapter3/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// fmt.Println("hoge")
+	var hoge int
+	bar := true
+
+	a := [3]string{
+		"Yamada",
+		"Sato",
+		"Suzuki",
+	}
+
+	fmt.Println(a)
+	fmt.Println(hoge)
+	fmt.Println(bar)
+
+}
